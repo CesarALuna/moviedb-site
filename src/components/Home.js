0fa968a53a60cfaba648b5eb451ac20708ee2c2e
CesarALuna <CesLun@gmail.com>
2021-08-30 21:55:18 -0700

    fix missing imports

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,12 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
 //components
 
 //hook
-
+import { useHomeFetch } from '../hooks/useHomeFetch'
 //image
 import NoImage from '../images/no_image.jpg'
 
 const Home = () => {
+  const { state, loading, error } = useHomeFetch()
   console.log(state)
   return <div>Home Page</div>
 }
