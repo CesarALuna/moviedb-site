0a1ed038f2d16be71113f77de5cb426597ec365d
CesarALuna <CesLun@gmail.com>
2021-09-01 15:52:15 -0700

    finish Routing Movie and useMovieFetch

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 import { Wrapper, Content, LogoImg, TMDBLogoImg } from './Header.styles'
 import RMDBLogo from '../../images/react-movie-logo.svg'
 import TMDBLogo from '../../images/tmdb_logo.svg'
@@ -6,7 +8,9 @@ import TMDBLogo from '../../images/tmdb_logo.svg'
 const Header = () => (
   <Wrapper>
     <Content>
-      <LogoImg src={RMDBLogo} alt="rmbd-logo" />
+      <Link to="/">
+        <LogoImg src={RMDBLogo} alt="rmbd-logo" />
+      </Link>
       <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo" />
     </Content>
   </Wrapper>
---
+++ src/components/Movie.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+// Configuration
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config'
+// components
+import Grid from './Grid'
+import Spinner from './Spinner'
+//hook
+import { useMovieFetch } from '../hooks/useMovieFetch'
+//image
+import NoImage from '../images/no_image.jpg'
+
+const Movie = () => {
+  const { movieId } = useParams()
+
+  const { state: movie, loading, error } = useMovieFetch(movieId)
+  console.log(movie)
+  return (
+    <>
+      <div>Something</div>
+    </>
+  )
+}
+
+export default Movie
