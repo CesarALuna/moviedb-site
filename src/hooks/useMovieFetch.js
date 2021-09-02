9ca388d4f67c38a7a9e010392e56ec79bd7854d9
CesarALuna <CesLun@gmail.com>
2021-09-02 15:58:53 -0700

    finish session storge for movie

--- src/hooks/useMovieFetch.js
+++ src/hooks/useMovieFetch.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 //API
 import API from '../API'
+//helpers
+import { isPersistedState } from '../helpers'
 
 export const useMovieFetch = (movieId) => {
   const [state, setState] = useState({})
@@ -31,8 +33,21 @@ export const useMovieFetch = (movieId) => {
         setError(true)
       }
     }
+    const sessionState = isPersistedState(movieId)
+
+    if (sessionState) {
+      setState(sessionState)
+      setLoading(false)
+      return
+    }
+
     fetchMovie()
   }, [movieId])
+
+  // write to session storage
+  useEffect(() => {
+    sessionStorage.setItem(movieId, JSON.stringify(state))
+  }, [movieId, state])
 
   return { state, loading, error }
 }
