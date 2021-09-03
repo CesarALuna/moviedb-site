befdbcf1f173bece1be6da1fee7b18ab5b331f06
CesarALuna <CesLun@gmail.com>
2021-09-03 15:22:20 -0700

    revert to functional components

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,44 +1,40 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 //image
 import searchIcon from '../../images/search-icon.svg'
 //styles
 import { Wrapper, Content } from './SearchBar.styles'
 
-class SearchBar extends Component {
-  state = { value: '' }
-  timeout = null
+const SearchBar = ({ setSearchTerm }) => {
+  const [state, setState] = useState('')
+  const initial = useRef(true)
 
-  componentDidUpdate(_prevProps, prevState) {
-    if (this.state.value !== prevState.value) {
-      const { setSearchTerm } = this.props
-
-      clearTimeout(this.timeout)
-
-      this.timeout = setTimeout(() => {
-        const { value } = this.state
-        setSearchTerm(value)
-      }, 500)
+  useEffect(() => {
+    if (initial.current) {
+      initial.current = false
+      return
     }
-  }
-  render() {
-    const { value } = this.state
-    return (
-      <Wrapper>
-        <Content>
-          <img src={searchIcon} alt="search-icon" />
-          <input
-            type="text"
-            placeholder="Search movies"
-            onChange={(event) =>
-              this.setState({ value: event.currentTarget.value })
-            }
-            value={value}
-          />
-        </Content>
-      </Wrapper>
-    )
-  }
+
+    const timer = setTimeout(() => {
+      setSearchTerm(state)
+    }, 500)
+
+    return () => clearTimeout(timer)
+  }, [setSearchTerm, state])
+
+  return (
+    <Wrapper>
+      <Content>
+        <img src={searchIcon} alt="search-icon" />
+        <input
+          type="text"
+          placeholder="Search movies"
+          onChange={(event) => setState(event.currentTarget.value)}
+          value={state}
+        />
+      </Content>
+    </Wrapper>
+  )
 }
 
 SearchBar.propTypes = {
